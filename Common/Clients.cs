e33aa96a640ec25aeba295620552523b6fe1c3e4
PC1 <PC1@DESKTOP-6IISJCV>
2022-05-04 11:10:29 +0300

    Client

--- Common/Clients.cs
+++ Common/Clients.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CarsDB_MVC.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,21 @@ namespace CarsDB_MVC.Common
             Name = null;
             Surname = null;
             LastName = null;
+        }
+        public void AddClient(string name, string surname, string lastname)
+        {
+            var con = Database.Connection();
+            con.Open();
+            using (con)
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "insert into Clients(Name,Surname,LastName) values (@name,@surname,@lastname);";
+                cmd.Connection = con;
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@surname", surname);
+                cmd.Parameters.AddWithValue("@lastname", lastname);
+                cmd.ExecuteNonQuery();
+            }
         }
         public override string ToString()
         {
